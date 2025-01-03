01b09fc77583a7a9ad0b2701507784c02de791ed
Ryley Watson <ryleyswatson@gmail.com>
2025-01-03 08:55:54 -0800

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+  const newHeader=document.createElement('h1');
+  newHeader.id="victory"
+
+ newHeader.innerHTML="Ryley Watson is the champion";
+ document.body.appendChild(newHeader);
+ 
